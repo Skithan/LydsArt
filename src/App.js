cbb28680d2d28a59e19864c817554f4cb3a2fa8f
Ethan Alward <148471703+Skithan@users.noreply.github.com>
2025-09-26 12:39:32 -0300

    deleted build folder

--- src/App.js
+++ src/App.js
@@ -4,20 +4,24 @@ import Header from './Header';
 import Footer from './Footer';
 
 function App() {
+  // Utility to format date string 'YYYYMMDD' to 'Month Day, Year'
+  function formatDate(dateStr) {
+    if (!dateStr || dateStr === 'N/A') return '';
+    if (dateStr.length < 8) return dateStr;
+    const year = dateStr.slice(0, 4);
+    const month = dateStr.slice(4, 6);
+    const day = dateStr.slice(6, 8);
+    const dateObj = new Date(`${year}-${month}-${day}`);
+    if (isNaN(dateObj)) return dateStr;
+    return dateObj.toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' });
+  }
   const cards = [
-    { img: process.env.PUBLIC_URL + '/Headshot.jpeg', title: 'Lydia Patterson', text: "Hi I'm Lydia.. contact info...", size: 'N/A', price: 'N/A' , date: 'N/A', sold: false},
-    { img: process.env.PUBLIC_URL + '/AnUptownPerspective2.jpeg',title: 'An Uptown Perspective', text: 'Two perspectives',size: '18"x24"',price: '$200' , date: '20250611', sold: false},
-    
-    { img: process.env.PUBLIC_URL + '/ComfortInChange.jpeg', 
-      title: 'Comfort In Change', text: 'sharing some thoughts from the painting process: boxes, full of pictures that tell stories, new and old, are packed and driven out of Sackville- away from the Cube, 204, and loud, dusty studios. boxes, unpacked into a house on Winslow, full of music, and dancing- decorated with undergraduate art, and yellow flowers. the windows of Winslow let the sun paint the walls with light from the morning sun and the small deck welcomes the last beams of the day. change settles as boxes are stored in the attic new pictures are taken, old ones are hung constant comfort is found each day in the setting of the sun.' , 
-      size: '24"x30"', 
-      price: '$200', 
-      date: '20240527', 
-      sold: false},
-
-    { img: process.env.PUBLIC_URL + '/EndOfSummerFlowers.jpeg', title: 'End Of Summer Flowers', text: 'End Of Summer Flowers' , size: '18"x24"', price: '$200', date: '20250810', sold: false},
-    { img: process.env.PUBLIC_URL + '/FalseLight.jpeg', title: 'False Light', text: 'Acrylic on Canvas', size: '18"x24"', price: '$200', date: '20250822', sold: false},
-    { img: process.env.PUBLIC_URL + '/FamiliarFaces.jpeg', title: 'Familiar Faces', text: 'Acrylic on Canvas', size: '18"x24"', price: 'N/A', date: '20250817', sold: false},
+    { img: process.env.PUBLIC_URL + '/Headshot.jpeg', title: 'Lydia Paterson', text: "Hi I'm Lydia.. contact info...", medium:'N/A', size: 'N/A', price: 'N/A' , date: 'N/A', sold: false},
+    { img: process.env.PUBLIC_URL + '/AnUptownPerspective2.jpeg',title: 'An Uptown Perspective', medium:'Acrylic', text: 'N/A',size: '18"x24"',price: '$200' , date: '20250611', sold: false},
+    { img: process.env.PUBLIC_URL + '/ComfortInChange.jpeg', title: 'Comfort In Change', text: 'sharing some thoughts from the painting process: ', medium: 'Oil on Stetched Canvas', size: '24"x30"', price: '$200', date: '20240527', sold: false},
+    { img: process.env.PUBLIC_URL + '/EndOfSummerFlowers.jpeg', title: 'End Of Summer Flowers', text: 'N/A', medium: 'Acrylic on Panel', size: '18"x24"', price: '$200', date: '20250810', sold: false},
+    { img: process.env.PUBLIC_URL + '/FalseLight.jpeg', title: 'False Light', text: 'N/A', medium: 'Acrylic on Canvas', size: '18"x24"', price: '$200', date: '20250822', sold: false},
+    { img: process.env.PUBLIC_URL + '/FamiliarFaces.jpeg', title: 'Familiar Faces', text: 'N/A', medium: 'Acrylic on Canvas', size: '18"x24"', price: 'N/A', date: '20250817', sold: false},
     { imgs: [
         process.env.PUBLIC_URL + '/SillyHeart1.jpeg',
         process.env.PUBLIC_URL + '/SillyHeart2.jpeg',
@@ -25,7 +29,8 @@ function App() {
         process.env.PUBLIC_URL + '/SillyHeart4.jpeg',
       ],
       title: 'Silly Hearts',
-      text: "Wood & Mixed Media",
+      text: 'N/A',
+      medium: 'Wood & Mixed Media',
       size: '1"x6"',
       price: '$200 each',
       date: '20230129',
@@ -36,18 +41,19 @@ function App() {
         process.env.PUBLIC_URL + '/HabitualRoutines2.jpeg',
     ],
       title: 'Habitual Routines',
-      text: 'Repurposed wood, Enamel, Ceramic Tiles',
+      text: 'N/A',
+      medium: 'Wood & Mixed Media',
       size: '3\'x4\'x2\'',
       price: '$200',
       date: '20231210',
       sold: false
     },
-    { img: process.env.PUBLIC_URL + '/KingSquareAtNight.jpeg', title: 'King Square At Night', text: 'Oil on Canvas' , size: '18"x24"', price: '$200',date: '2022052', sold: false},
-    { img: process.env.PUBLIC_URL + '/NoDogsOnTheCouch.jpeg', title: 'No Dogs On The Couch', text: 'Oil on Canvas' , size: '18"x24"', price: 'N/A',date: '20221201', sold: false},
-    { img: process.env.PUBLIC_URL + '/SharingATemporaryHome.jpeg', title: 'Sharing A Temporary Home', text: 'Acrylic on Canvas' , size: '3\'x5\'', price: '$200',date: '20231103', sold: false},
-    { img: process.env.PUBLIC_URL + '/SimonAndGarfunkelKids.jpeg', title: 'Simon And Garfunkel Kids', text: 'Simon And Garfunkel Kids', size: 'N/A', price: 'N/A',date: '20240822', sold: false},
-    { img: process.env.PUBLIC_URL + '/YellowFrog.jpeg', title: 'Yellow Frog', text: 'Yellow Frog', size: 'N/A', price: 'N/A',date: '20240219', sold: false},
-    { img: process.env.PUBLIC_URL + '/BlueFrog.jpeg', title: 'Blue Frog', text: 'Blue Frog',size: 'N/A', price: 'N/A',date: '20240219', sold: false},
+    { img: process.env.PUBLIC_URL + '/KingSquareAtNight.jpeg', title: 'King Square At Night', text: 'N/A', medium: 'Oil on Canvas', size: '18"x24"', price: '$200',date: '2022052', sold: false},
+    { img: process.env.PUBLIC_URL + '/NoDogsOnTheCouch.jpeg', title: 'No Dogs On The Couch', text: 'N/A', medium: 'Oil on Canvas', size: '18"x24"', price: 'N/A',date: '20221201', sold: false},
+    { img: process.env.PUBLIC_URL + '/SharingATemporaryHome.jpeg', title: 'Sharing A Temporary Home', text: 'N/A', medium: 'Acrylic on Canvas', size: '3\'x5\'', price: '$200',date: '20231103', sold: false},
+    { img: process.env.PUBLIC_URL + '/SimonAndGarfunkelKids.jpeg', title: 'Simon And Garfunkel Kids', text: 'N/A', medium: 'Acrylic on Canvas', size: 'N/A', price: 'N/A',date: '20240822', sold: false},
+    { img: process.env.PUBLIC_URL + '/YellowFrog.jpeg', title: 'Yellow Frog', text: 'N/A', medium: 'Oil on Canvas', size: 'N/A', price: 'N/A',date: '20240219', sold: false},
+    { img: process.env.PUBLIC_URL + '/BlueFrog.jpeg', title: 'Blue Frog', text: 'N/A', medium: 'Oil on Canvas', size: 'N/A', price: 'N/A',date: '20240219', sold: false},
   ];
   const [current, setCurrent] = useState(0);
   const [expanded, setExpanded] = useState(false);
@@ -150,7 +156,7 @@ function App() {
                 onClick={handleExpand}
                 style={{ cursor: 'pointer', transition: 'all 0.7s cubic-bezier(.77,0,.18,1)' }}
               >
-                {cards[current].title}
+                <div className="card-text">{cards[current].title}</div>
               </div>
               <button className="arrow-btn right" onClick={goRight} aria-label="Next card">&#8594;</button>
             </div>
@@ -167,7 +173,13 @@ function App() {
 
               {cards[current].text !== 'N/A' ? <div className="card-detail-row">{cards[current].text}</div> : null}
               {cards[current].size !== 'N/A' ? <div className="card-detail-row"><strong>Size:</strong> {cards[current].size}</div> : null}
+              {cards[current].medium !== 'N/A' ? <div className="card-detail-row"><strong>Medium:</strong> {cards[current].medium}</div> : null}
               {cards[current].price !== 'N/A' ? <div className="card-detail-row"><strong>Price:</strong> {cards[current].price}</div> : null}
+                {cards[current].date && cards[current].date !== 'N/A' ? (
+                  <div className="card-detail-row"><strong>Date Completed:</strong> {formatDate(cards[current].date)}</div>
+                ) : null}
+
+
             </div>
             <div className="card-scrollbar">
               {cards.map((_, idx) => (
